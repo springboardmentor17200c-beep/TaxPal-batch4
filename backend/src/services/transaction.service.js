7d219d259fb307cf1fd810c83f2a3c0dc0e2ad00
malmarugan05 <malmarugan05@gmail.com>
2026-02-11 22:43:50 +0530

    Update transaction.service.js
    Transaction Model MongoDB +Mongoose

---
+++ backend/src/services/transaction.service.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // adds createdAt & updatedAt automatically
+  }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
