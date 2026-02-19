fa4292059f94ca05f0129e3aea8f002abeecb23e
malmarugan05 <malmarugan05@gmail.com>
2026-02-19 13:47:18 +0530

    Update app.js
    Added
    
    const budgetRoutes = require("./routes/budget.routes");
    app.use("/budgets", budgetRoutes);

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const authRoutes = require("./routes/auth.routes");
 const errorMiddleware = require("./middlewares/error.middleware");
 const dashboardRoutes = require("./routes/dashboard.routes");
 const transactionRoutes = require("./routes/transaction.routes");
+const budgetRoutes = require("./routes/budget.routes");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/transactions", transactionRoutes);
+app.use("/api/budgets", budgetRoutes);
 
 // 404 handler
 app.use((req, res, next) => {
